eb491e826fcf2ddeee1161962cdd6ab77a5e2c46
john.a.clark3 <john.a.clark3@gmail.com>
2022-02-17 15:47:49 -0700

    Working on name

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,19 @@
+const Employee = require("../lib/Employee.js");
 
+// name
+test("creates an employee object", () => {
+  const employee = new Employee("Dave");
+
+  expect(employee.name).toBe("Dave");
+});
+// id
+
+// email
+
+// getName()
+
+// getId()
+
+// getEmail()
+
+// getRole() // Returns 'Employee'
