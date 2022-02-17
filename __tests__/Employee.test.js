0d4ca97a172c30e98a0670f4eae01ba3c1d23999
john.a.clark3 <john.a.clark3@gmail.com>
2022-02-17 16:18:15 -0700

    getName() up a running

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,19 +1,27 @@
 const Employee = require("../lib/Employee.js");
 
-// name
 test("creates an employee object", () => {
+  const employee = new Employee("Dave", 1, "dave@yahoo.com");
+  // name
+  expect(employee.name).toBe("Dave");
+  // id
+  expect(employee.id).toEqual(expect.any(Number));
+  // email
+  expect(employee.email).toBe("dave@yahoo.com");
+});
+// getName()
+test("gets employee name", () => {
   const employee = new Employee("Dave");
 
-  expect(employee.name).toBe("Dave");
+  expect(employee.getName()).toEqual(
+    expect.stringContaining(employee.name.toString())
+  );
 });
-// id
-
-// email
-
-// getName()
 
 // getId()
 
 // getEmail()
 
 // getRole() // Returns 'Employee'
+
+console.log(new Employee("Mira", 12, "thepups@gmail.com"));
