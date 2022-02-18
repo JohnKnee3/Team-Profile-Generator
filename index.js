2da9367a2183bc1f53259d8bb62358e386aa968e
john.a.clark3 <john.a.clark3@gmail.com>
2022-02-18 16:29:18 -0700

    added require fs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
